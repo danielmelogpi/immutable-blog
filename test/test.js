4dac8a658d21869a206444d9d7af90d6cb2b6075
Daniel Melo <danielmelogpi@gmail.com>
2021-04-18 16:44:14 -0300

    add test for nickname set and reset

--- test/test.js
+++ test/test.js
@@ -1,19 +1,20 @@
-const ImmutableBlogContract = artifacts.require('./ImmutableBlog.sol')
+const ImmutableBlog = artifacts.require('./ImmutableBlog.sol')
 
 require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('ImmutableBlog', ([deployer, author, tipper]) => {
-  let ImmutableBlog
-
+contract('ImmutableBlog', (accounts) => {
+  let blog
+  let firstAccount
   before(async () => {
-    ImmutableBlog = await ImmutableBlogContract.deployed()
+    blog = await ImmutableBlog.deployed()
+    firstAccount = accounts[0]
   })
 
   describe('deployment', async () => {
     it('deploys successfully', async () => {
-      const address = await ImmutableBlog.address
+      const address = await blog.address
       assert.notEqual(address, 0x0)
       assert.notEqual(address, '')
       assert.notEqual(address, null)
@@ -21,8 +22,25 @@ contract('ImmutableBlog', ([deployer, author, tipper]) => {
     })
 
     it('has a name', async () => {
-      const name = await ImmutableBlog.name()
+      const name = await blog.name()
       assert.equal(name, 'ImmutableBlog')
+    })
+  })
+
+  describe('user profile operations', async () => {
+    const myNickname = 'GoldenGorila'
+    before(async () => {
+      await blog.setMyNickname(myNickname)
+    })
+    it ('is able to set a nickname for the current user', async () => {
+      const currentNickname = (await blog.authors(firstAccount)).nickname
+      assert.equal(myNickname, currentNickname)
+    })
+    it ('is able to change a nickname for the current user', async () => {
+      const newNickname = 'BlackManta'
+      await blog.setMyNickname(newNickname)
+      const currentNickname = (await blog.authors(firstAccount)).nickname
+      assert.equal(newNickname, currentNickname)
     })
   })
 
